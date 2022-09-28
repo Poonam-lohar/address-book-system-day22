686bd4c00d1ec54cb2aa35e625234f9d8fed9c67
Your Name <you@example.com>
2022-09-28 11:54:23 +0530

    added:Welcome to address book system

---
+++ src/com/collection/AddressBook.java
@@ -0,0 +1,30 @@
+package com.collection;
+
+public class AddressBook {
+    /**
+     * create a main method ,all program execute in main method
+     * @param args no arguments
+     */
+
+    public static void main(String[] args) {
+        /**
+         * 1st print welcome msg.
+         */
+        System.out.println("Welcome to Address Book System\n");
+        /**
+         * We are just calling an object using class name.
+         * Simply passing the argument value according to the parameter defined in the constructor.
+         */
+        Contact details = new Contact("Poonam", "Desai", "Shirol", "Shirol",
+                "Maharashtra", "loharpoonam98@gmail.com", 416103, 7058835985l);
+        System.out.println("The following contact details is mentioned below : \n");
+        System.out.println("First Name : " + details.getFirstName());
+        System.out.println("Last Name  : " + details.getLastName());
+        System.out.println("Address    : " + details.getAddress());
+        System.out.println("City       : " + details.getCity());
+        System.out.println("State      : " + details.getState());
+        System.out.println("E-mail     : " + details.getEmail());
+        System.out.println("Zip Code   : " + details.getZip());
+        System.out.println("Phone No   : " + details.getPhoneNumber());
+    }
+}
