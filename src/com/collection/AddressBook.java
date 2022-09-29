f4ff077d57ed3f48c852827c7c0b984eb7297712
Your Name <you@example.com>
2022-09-29 11:05:14 +0530

    added:logic for delete details

--- src/com/collection/AddressBook.java
+++ src/com/collection/AddressBook.java
@@ -2,10 +2,24 @@ package com.collection;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class AddressBook {
+    /**
+     * create a array list object
+     * The ArrayList class of the Java collections framework provides the functionality of resizable-arrays.
+     * It implements the List interface.
+     */
     ArrayList<Contact> arrayDetails = new ArrayList<Contact>();
     static Scanner sc = new Scanner(System.in);
+
+    /**
+     * This method is used to add details to address book
+     */
     public void addDetails() {
+        /**
+         * create a object an contacts class.
+         * object name is info
+         */
         Contact info = new Contact();
+
         System.out.println("Enter the first name");
         info.setFirstName(sc.nextLine());
         System.out.println("Enter the last name");
@@ -23,20 +37,31 @@ public class AddressBook {
         System.out.println("Enter the phone number");
         info.setPhoneNumber(sc.nextLong());
         arrayDetails.add(info);
-        //sc.close();
     }
+    /**
+     * create a method name as editDetails.
+     * This method is used to edit the details in address book
+     */
     public void editDetails() {
         System.out.println("Confirm your first name to edit details: ");
         String confirmName = sc.next();
+        /**
+         * using for loop
+         * a)i=0=initialize; b)i < arrayDetails.size()=condition; c)i++= increment by 1
+         *
+         * 1) 1st initialize the value of a variable,then check the condition,then go to if condiion,in this condition arrayDetails object
+         *   the element at the specified position in this list then if they are equals then select the option
+         *   choose edit detatils then 1st confirm ur first name to edit details then put ur previous name what u set this,
+         *   then select what u want change,suppose u choose first name,
+         *   then put it new name what u want then its edit and after the print all the information with editing new name
+         *   then again update.
+         *
+         */
         for (int i = 0; i < arrayDetails.size(); i++) {
             if (arrayDetails.get(i).getFirstName().equals(confirmName)) {
                 System.out.println("Select form below to change: ");
                 System.out.println("\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Mobile number\n8.Email");
                 int edit = sc.nextInt();
-                sc.close();
-                /**
-                 * switch case is used for what u want edit
-                 */
                 switch (edit) {
                     case 1:
                         System.out.println("Enter first name");
@@ -71,27 +96,86 @@ public class AddressBook {
                         arrayDetails.get(i).setEmail(sc.next());
                         break;
                 }
+                /**
+                 * if you will check the name which you have written earlier,whether it matches or not ,
+                 * if it matches then it will be edited after that.
+                 * if  you put previous name is incorrect then show the else statement
+                 */
                 System.out.println("Edited list is: ");
                 System.out.println(arrayDetails);
             } else
                 System.out.println("Enter a valid First name");
         }
+
     }
+    /**
+     * create a method name as deleteDetails
+     * This method is used to delete the contact details
+     */
+    public void deleteDetails() {
+        System.out.println("Confirm the first name of the person to delete contact");
+        //user input
+        String confirmName = sc.next();
+        /**
+         * using for loop
+         * 1) 1st initialize the value of a variable,then check the condition,then go to if condition,in this condition arrayDetails object
+         *   the element at the specified position check in object then check they are equal or not if condtion is true then delete the element
+         *   in arrayDetails
+         */
+        for (int i = 0; i < arrayDetails.size(); i++) {
+
+            if (arrayDetails.get(i).getFirstName().equals(confirmName)) {
+                arrayDetails.remove(i);
+                System.out.println("List After removing"+arrayDetails);
+
+                /**
+                 * if condition is false then print enter valid first name
+                 */
+            } else {
+                System.out.println("Enter valid first name");
+            }
+        }
+    }
+
+    /**
+     * create a main method all program execute in main method
+     * @param args no arguments
+     */
     public static void main(String[] args) {
+        /**
+         * create a object name as details for  AddressBookSystem  class
+         * create object bcoz AddressBookSystem in this class all method is non static
+         */
         AddressBook details = new AddressBook();
+        /**
+         * calling method to object name . method name
+         * object name.method name;
+         * object=details
+         * method=addDetails(),display()
+         */
         details.addDetails();
+
         int i = 0;
         while (i == 0) {
+            /**
+             * 1st print the welcome msg.
+             */
             System.out.println("Welcome to Address Book Program");
             System.out.println("What do you want to do: ");
-            System.out.println("1.Add details.\n2.Edit details.");
+            System.out.println("1.Add details.\n2.Edit details.\n3.Delete Details.");
             int choose = sc.nextInt();
+            /**
+             * calling method in switch case
+             */
             switch (choose) {
                 case 1:
                     details.addDetails();
                     break;
                 case 2:
                     details.editDetails();
+                    break;
+                case 3:
+                    details.deleteDetails();
                     break;
                 default:
                     i = 1;
